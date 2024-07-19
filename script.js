9ea1db1b113ce8348bdd1b3a7d62f82136654624
virgile.dlr <virgile.dlr@protonmail.com>
2024-07-19 14:13:08 +0900

    adding changes

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 // script.js
 const translations = {
     en: {
-        title: "Welcome to ViDLR's GitHub Pages Site",
+        title: "Welcome to my page !",
         menuAbout: "About Me",
         menuProjects: "Projects",
         menuContact: "Contact",
+        menuDownload: "Downloads",
         aboutTitle: "About Me",
         aboutContent: "Hello! My name is Virgile de La Rochefoucauld, and I am passionate about robotics and trans-media multi-robot systems. I am currently pursuing a double degree program between Japan and France, where I have the opportunity to work on cutting-edge research in robotics and artificial intelligence.",
         projectsTitle: "Projects",
@@ -13,10 +14,11 @@ const translations = {
         contactContent: "Ways to get in touch with you."
     },
     fr: {
-        title: "Bienvenue sur le site GitHub Pages de ViDLR",
+        title: "Bienvenue sur le ma page !",
         menuAbout: "À propos de moi",
         menuProjects: "Projets",
         menuContact: "Contact",
+        menuDownload: "Téléchargements",
         aboutTitle: "À propos de moi",
         aboutContent: "Bonjour! Je m'appelle Virgile de La Rochefoucauld et je suis passionné par la robotique et les systèmes multi-robots trans-média. Je poursuis actuellement un programme de double diplôme entre le Japon et la France, où j'ai l'opportunité de travailler sur des recherches de pointe en robotique et en intelligence artificielle.",
         projectsTitle: "Projets",
@@ -25,10 +27,11 @@ const translations = {
         contactContent: "Moyens de vous contacter."
     },
     jp: {
-        title: "ViDLRのGitHubページへようこそ",
+        title: "私のページへようこそ",
         menuAbout: "私について",
         menuProjects: "プロジェクト",
         menuContact: "連絡先",
+        menuDownload: "ダウンロード",
         aboutTitle: "私について",
         aboutContent: "こんにちは！私はヴィルジル・ド・ラ・ロシュフォコーで、ロボティクスとトランスメディアマルチロボットシステムに情熱を持っています。現在、私は日本とフランスの間でダブルディグリープログラムを追求しており、ロボティクスと人工知能の最先端研究に取り組む機会を得ています。",
         projectsTitle: "プロジェクト",
@@ -44,6 +47,7 @@ function changeLanguage() {
     document.getElementById("menu-about").textContent = translations[language].menuAbout;
     document.getElementById("menu-projects").textContent = translations[language].menuProjects;
     document.getElementById("menu-contact").textContent = translations[language].menuContact;
+    document.getElementById("menu-download").textContent = translations[language].menuDownload;
     document.getElementById("about-title").textContent = translations[language].aboutTitle;
     document.getElementById("about-content").textContent = translations[language].aboutContent;
     document.getElementById("projects-title").textContent = translations[language].projectsTitle;
